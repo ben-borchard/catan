8d1d5956905c48f388b0f67498b1ecba469d9cf2
Ben Borchard <ben.borchard@gmail.com>
2025-06-01 21:37:28 -0400

    cleanup

--- src/game/board/corner.py
+++
@@ -1,6 +0,0 @@
-
-class Corner:
-    """Intersection point of up to 3 tiles"""
-
-    def __init__(self):
-        self.building: Optional['Building'] = None  # Settlement or City
--- src/game/board/edge.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-
-class Edge:
-    """Connects two corners; a road can be built here"""
-
-    def __init__(self):
-        self.owner: Optional['Player'] = None
